2b3ba1f492bf3383e3b20773ebfa23a431e44780
Hossam Ayman <hhasanelbadry2000@gmail.com>
2026-01-08 20:51:32 +0200

    part 4 add another gRPC client to receive async stub and response on SseEmitter

--- src/main/java/org/example/stocktradingclient/controller/StockClientController.java
+++ src/main/java/org/example/stocktradingclient/controller/StockClientController.java
@@ -2,10 +2,12 @@ package org.example.stocktradingclient.controller;
 
 import org.example.stocktradingclient.dto.StockResponseDTO;
 import org.example.stocktradingclient.service.StockClientService;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
 
 @RestController
 @RequestMapping("/api/stocks")
@@ -17,8 +19,13 @@ public class StockClientController {
         this.stockClientService = stockClientService;
     }
 
-    @GetMapping
+    @GetMapping("/get/stock/price")
     public StockResponseDTO getStockPrice(@RequestParam(name = "symbol") String stockSymbol) {
         return stockClientService.getStockPrice(stockSymbol);
+    }
+
+    @GetMapping(value = "/subscribe/stock/price",produces = MediaType.TEXT_EVENT_STREAM_VALUE)
+    public SseEmitter subscribeStockPrice() {
+        return stockClientService.subscribeStockPrice();
     }
 }
--- src/main/java/org/example/stocktradingclient/service/StockClientService.java
+++ src/main/java/org/example/stocktradingclient/service/StockClientService.java
@@ -3,9 +3,12 @@ package org.example.stocktradingclient.service;
 import com.example.grpc.StockRequest;
 import com.example.grpc.StockResponse;
 import com.example.grpc.StockTradingServiceInterfaceGrpc;
+import com.google.protobuf.Empty;
+import io.grpc.stub.StreamObserver;
 import net.devh.boot.grpc.client.inject.GrpcClient;
 import org.example.stocktradingclient.dto.StockResponseDTO;
 import org.springframework.stereotype.Service;
+import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
 
 @Service
 public class StockClientService {
@@ -13,8 +16,12 @@ public class StockClientService {
     //blocking stub is correct fit for unary
     //because we're relying on request / response model
     //note that blocking stub you cannot use for streaming client or bidirectional streaming
-    @GrpcClient("stockStubService")
+    @GrpcClient("stockBlockStubService")
     private StockTradingServiceInterfaceGrpc.StockTradingServiceInterfaceBlockingStub stockBlockingStub;
+
+    //non-blocking stub
+    @GrpcClient("stockStubService")
+    private StockTradingServiceInterfaceGrpc.StockTradingServiceInterfaceStub stockAsyncStub;
 
     public StockResponseDTO getStockPrice(String stockSymbol) {
         StockRequest request = StockRequest.newBuilder()
@@ -22,6 +29,39 @@ public class StockClientService {
                 .build();
         StockResponse response = stockBlockingStub.getStockPrice(request);
         return new StockResponseDTO(response.getStockSymbol(), response.getPrice(), response.getTimestamp());
+    }
+
+    public SseEmitter subscribeStockPrice() {
+        SseEmitter emitter = new SseEmitter();
+
+        Empty emptyRequest = Empty.newBuilder().build();
+        stockAsyncStub.subscribeStockPrice(emptyRequest, new StreamObserver<StockResponse>() {
+            @Override
+            public void onNext(StockResponse stockResponse) {
+                try {
+                    StockResponseDTO dto = new StockResponseDTO(
+                            stockResponse.getStockSymbol(),
+                            stockResponse.getPrice(),
+                            stockResponse.getTimestamp()
+                    );
+                    emitter.send(dto);
+                } catch (Exception e) {
+                    emitter.completeWithError(e);
+                }
+            }
+
+            @Override
+            public void onError(Throwable throwable) {
+                emitter.completeWithError(throwable);
+            }
+
+            @Override
+            public void onCompleted() {
+                emitter.complete();
+            }
+        });
+
+        return emitter;
     }
 
 }
